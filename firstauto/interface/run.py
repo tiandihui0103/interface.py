5fe4975ee8634177d5b3532b67f27bf78df720de
Jeffrey <Jeffrey@LGroup.global>
2019-04-08 10:38:37 +0800

    去除没用的配置文件

--- firstauto/interface/run.py
+++ firstauto/interface/run.py
@@ -1,8 +1,7 @@
-import config,json,unittest,HTMLTestRunner,sys
+import json,unittest,HTMLTestRunner,sys
 from helper import request,unit
 from helper.api_v1 import api,http
 from testcase.api_v1 import login
-conf = config.Conf
 testunit = unittest.TestSuite();
 testunit.addTest(unittest.makeSuite(login.Loginin))
 fw = open('C:/Python35/Webx_Report/sincaitestreport_.html', 'wb')
